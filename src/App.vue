30757681df9131308627b6230b319bc495c52bb5
Charlon Palacay <charlon@gmail.com>
2020-12-11 15:12:27 -0800

    Updating boilerplate template.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="border-solid border-4 border-light-blue p-12 text-center">
-    <header>
-      <img alt="Vue logo" src="./assets/logo.png" class="inline" />
-      <HelloWorld msg="Hello Vue 3.0 + Vite" />
-    </header>
+  <header class="border-solid border-4 border-light-blue p-12 text-center">
+    <img alt="Vue logo" src="./assets/logo.png" class="inline" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
+  </header>
+  <main class="p-12">
     <router-view />
-    <footer>this is the footer</footer>
-  </div>
+  </main>
+  <footer class="border-solid border-4 border-light-blue p-6 text-center">
+    this is the footer
+  </footer>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,9 @@
 <template>
   <h1>Home Page</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quidem
+    natus repudiandae iure repellat voluptatum voluptatibus. Rerum deserunt
+    distinctio at beatae, tenetur molestias animi praesentium est omnis quasi
+    nihil recusandae.
+  </p>
 </template>
