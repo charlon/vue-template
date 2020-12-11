c510a6df8b88cb7a4bc5fcd8bd570394a76373ff
Charlon Palacay <charlon@gmail.com>
2020-12-11 15:01:56 -0800

    Adds vue-router and wires up home and about pages.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="border-solid border-4 border-light-blue p-12 text-center">
-    <img alt="Vue logo" src="./assets/logo.png" class="inline" />
-    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <header>
+      <img alt="Vue logo" src="./assets/logo.png" class="inline" />
+      <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    </header>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+    <footer>this is the footer</footer>
   </div>
 </template>
 
