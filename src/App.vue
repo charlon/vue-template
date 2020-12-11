d9432894d705a8c004e2a229a8956fabeadabc57
Charlon Palacay <charlon@gmail.com>
2020-12-11 12:57:12 -0800

    Adds Tailwind and PostCSS.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div class="border-solid border-4 border-light-blue p-12 text-center">
+    <img alt="Vue logo" src="./assets/logo.png" class="inline" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
