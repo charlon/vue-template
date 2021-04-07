166265e138bcf3c6c038edcb2b3d89902a9f7377
Charlon Palacay <charlon@gmail.com>
2021-04-07 11:00:56 -0700

    Added comment.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' 
 
-// bootstrap 4
+// bootstrap 4 (standalone)
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
