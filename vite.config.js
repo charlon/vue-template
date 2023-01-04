d422103ee9011a9e630e7cad80644724c4590696
Charlon Palacay <charlon@gmail.com>
2023-01-03 17:26:58 -0800

    Updates package.json

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from "url";
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
@@ -5,4 +7,9 @@ import WindiCSS from "vite-plugin-windicss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), WindiCSS()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
